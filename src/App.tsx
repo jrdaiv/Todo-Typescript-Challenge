e6524a1657f21f28eeeb7c6846281458a6c29dd7
jrdaiv <jrdaiv@gmail.com>
2024-08-26 14:13:06 -0700

    deleted useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import User from "./components/User"
 import TodoPage from "./components/TodoPage";
 
 
 export const App: React.FC = () => {
-  // const [user, setUser] = useState<string>('');
-
-  // if (!localStorage.getItem('user')) {
-  //   return (
-  //     <User />
-  //   )
-  // }
-
 
   return (
 
